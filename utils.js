0582debdd76aaf31965f0a48b9caada0302a2649
xcy <413508034@qq.com>
2016-04-12 17:56:21 +0800

    Modify the bug of the uitls.js

--- utils.js
+++ utils.js
@@ -1,65 +1,69 @@
-var doc;
+;(function (global) {
+  global.utils = {
+    doc: null,
+    isHaveShift: function(key) {
+      for(var i in key) {
+        if(key[i] === 16) {
+          key.splice(key[i], 1);
+          return true;
+        }
+      }
+      return false;
+    },
+    bind: function(element, key, callback) {
+      if(!element || !key) {
+        throw new Error('参数不能为空');
+      }
 
-var utils = {
-  bind: bind,
-  unbind: unbind,
-  copy: copyElement,
-  save: enableAutoSave,
-  load: enableLoadData
-}
+      doc = element.addEventListener('keydown', function(event) {
 
-function bind(element, key, callback) {
-  if(!element || !key) {
-    throw new Error('参数不能为空');
-  }
+        if(key.length === 1) {
+          if(event.shiftKey === false && event.keyCode === key[0]) {
+            callback.call(null, this);
+          }
+        } else if(key.length > 1) {
+          if(this.isHaveShift(key)) {
+            if(event.shiftKey === true && event.keyCode === key[0]) {
+              callback.call(null, this);
+            }
+          }
+        }
+        // end if
+      })
 
-  doc = element.addEventListener('keydown', function(event) {
-    if(event.shiftKey === false && event.keyCode === key[0]) {
-      console.log('tab')
-      callback.call(null, this);
+      return callback;
+    },
+
+    unbind: function (element, key, callback) {
+      element.removeEventListener('keydown', this.doc);
+
+      return callback;
+    },
+
+    copyElement: function (element) {
+      var range = document.createRange();
+      range.selectNodeContents(element);
+      window.getSelection().addRange(range);
+
+      try {
+        var style = document.execCommand('copy');
+        var msg = style ? '成功' : '失败';
+        alert('拷贝'+msg);
+      } catch(err) {
+        alert('不能拷贝');
+      }
+      window.getSelection().removeAllRanges();
+    },
+
+    enableAutoSave: function (gap) {
+      setInterval(function() {
+        localStorage.setItem('saves', table.export());
+      }, gap * 1000);
+    },
+
+    enableLoadData: function () {
+      return localStorage.getItem('saves');
     }
-
-    if(event.shiftKey === true && event.keyCode === key[0]) {
-      console.log('shift+tab')
-      callback.call(null, this);
-    }
-  })
-
-  return {
-    callback: callback
   }
-}
+})(window);
 
-function unbind(element, key, callback) {
-  element.removeEventListener('keydown', doc);
-  return {
-    callback: callback
-  }
-}
-
-function copyElement(element) {
-  var range = document.createRange();
-  range.selectNodeContents(element);
-  window.getSelection().addRange(range);
-
-  try {
-    var style = document.execCommand('copy');
-    var msg = style ? '成功' : '失败';
-    alert('拷贝'+msg);
-  } catch(err) {
-    alert('不能拷贝');
-  }
-  window.getSelection().removeAllRanges();
-}
-
-function enableAutoSave(data, gap) {
-  setInterval(function() {
-    localStorage.setItem('message', data);
-  }, gap * 1000);
-}
-
-function enableLoadData() {
-  return {
-    data: localStorage.getItem('message')
-  }
-}
