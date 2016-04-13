55474e28f410a7590024942b4bbf89c92455f1e9
Hellocyy <Hellocyy94824@163.com>
2016-04-13 09:49:17 +0800

    Modify table.js

--- table.js
+++ table.js
@@ -1,101 +1,207 @@
-var table = document.getElementById('table');
-var Table = {
-  //初始化表格
-  init: function(table,props){
-    var tr = document.createElement('tr');
-    table.appendChild(tr);
-    for(var a = 0; a<props.title.length; a++){
-      var th = document.createElement('th');
-      th.innerHTML = props.title[a];
-      tr.appendChild(th);
-    }
-    for(var i = 0; i < props.row.length; i++){
+;(function(global) {
+  global.init = function(box,props) {
+    for(var x = 0;x < props.length;x++) {
+
+      //表格
+      var table = document.createElement('table');
+      box.appendChild(table);
+
+      //标题
       var tr = document.createElement('tr');
       table.appendChild(tr);
-      for(var a = 0; a < props.title.length; a++){
+      var td = document.createElement('td');
+      td.className="table-header";
+      td.setAttribute("colspan",props[x].title.length);
+      td.innerHTML = props[x].head;
+      tr.appendChild(td);
+
+      //表头
+      var tr = document.createElement('tr');
+      table.appendChild(tr);
+      for(var a = 0; a<props[x].title.length; a++) {
         var td = document.createElement('td');
-        td.innerHTML = props.row[i][a];
+        td.className="table-title";
+        td.innerHTML = props[x].title[a];
         tr.appendChild(td);
       }
+
+      //数据
+      if (props[x].row.length == 0) {
+        var tr = document.createElement('tr');
+        table.appendChild(tr);
+        for(var a = 0; a < props[x].title.length; a++) {
+          var td = document.createElement('td');
+          tr.appendChild(td);
+          write(td);
+        }
+      }else{
+        for(var i = 0; i < props[x].row.length; i++) {
+          var tr = document.createElement('tr');
+          table.appendChild(tr);
+          var td = document.createElement('td');
+          tr.appendChild(td);
+          for(var a = 0; a < props[x].title.length-1; a++) {
+            var td = document.createElement('td');
+            td.innerHTML = props[x].row[i][a];
+            tr.appendChild(td);
+            write(td);
+          }
+        }
+      }
     }
-  },
+
+    //测使用
+    // document.onkeydown = function(ev) {
+    //   ev = ev || event;
+    //   if(ev.keyCode == 13) {
+    //     var cell = getFocusCell(box);
+    //     // createNewLine(cell);//创建新的一行
+    //     // focusPrevCell(box);//移到上一个单元格
+    //     // focusNextCell(box);//移到下一个单元格
+    //     // exportData(box,props);//导出表格数据
+    //   }
+    // }
+    return {
+      focusPrevCell: function(el) {
+        focusPrevCell(el);
+      },
+      focusNextCell: function(el) {
+        focusNextCell(el);
+      },
+    }
+  };
+
+  //获取聚焦单元格
+  function getFocusCell(el) {
+    var input = (el.getElementsByTagName('input') || [])[0];
+    return input?input.parentNode: input;
+  }
+
+  //查找父级表格
+  function findTableParent(cell) {
+    if (cell) {
+      parent = cell.parentNode;
+      if (parent && parent.tagName === 'TABLE') {
+        return parent;
+      } else {
+        return findTableParent(parent);
+      }
+    } else {
+      return undefined;
+    }
+  }
+
   //创建新的一行
-  createNewLine : function(props){
+  function createNewLine(cell) {
     var tr = document.createElement('tr');
+    var table = findTableParent(cell);
     table.appendChild(tr);
-    for(var a = 0; a < props.title.length; a++){
+    var length = table.getElementsByTagName('tr')[1].getElementsByTagName('td').length;
+    for(var a = 0; a < length; a++) {
       var td = document.createElement('td');
+      write(td);
       tr.appendChild(td);
     }
-  },
-  //focus
-  focus : function(cell){
-    cell.value=cell.innerHTML;
-    var input = document.createElement('input');
-    cell.appendChild(input);
-    input.focus();
-    input.type='text';
-    input.value=cell.value;
-  },
-  //unfocus
-  unfocus : function(cell){
-    var input = cell.getElementsByTagName('input')[0];
-    cell.innerHTML = input.value;
-    input.blur();
-    input.parentNode.removeChild(input);
-  },
-  focusNextCell : function(cell){
-    Table.unfocus(cell);
-    if(cell.nextSibling.nextSibling == null){
-      if(cell.parentNode.nextSibling.nextSibling==null){//如果是最后一行
-        Table.createNewLine(props);
-      }else{
-        var next = cell.parentNode.
-        nextSibling.nextSibling.childNodes.item(1);
-        if(oneone == null){
-          return;
-        }else {
-          Table.focus(next)
-        }
+  }
+
+  // 编辑数据
+  function write(cell) {
+    cell.addEventListener('click',function(ev) {
+      var input = document.createElement('input');
+      ev = ev || event;
+      if (ev.srcElement ===this) {
+        var value = cell.innerHTML;
+        cell.appendChild(input);
+        input.value = value;
+        input.focus();
+        input.addEventListener('blur',function() {
+          setTimeout(function () {
+            cell.innerHTML = input.value;
+          }, 1);
+          cell.removeChild(input);
+        });
       }
-    }else{
-      Table.focus(cell.nextSibling.nextSibling)
+    })
+  }
+
+  //将输入框移到下一个单元格
+  function focusNextCell(el) {
+    var cell = getFocusCell(el);
+    if (cell) {
+      if (cell.nextSibling == null) {
+        if (cell.parentNode.nextSibling==null) {//如果是最后一行
+          return;
+        }else{
+          var next = cell.parentNode.nextSibling.childNodes.item(1);
+          next.click();
+        }
+      }else{
+        cell.nextSibling.click();
+      }
     }
-  },
+  }
+
   //将输入框移到上一个单元格
-  focusPrevCell : function(cell){
-    Table.unfocus(cell);
-    if(cell.previousSibling.previousSibling == null){//如果是每行第一个
-      if(cell.parentNode.previousSibling==null){//如果是第一行
+  function focusPrevCell(el) {
+    var cell = getFocusCell(el);
+    console.log(cell.parentNode.previousSiblings);
+    if (cell.previousSibling.previousSibling == null) {//如果是每行第一个
+      if (cell.parentNode.previousSibling==null) {//如果是第一行
         return;
       }else{
-        var previous = cell.parentNode.previousSibling.previousSibling.lastChild.previousSibling;
-        if(previous == null){
+        var previous = cell.parentNode.previousSibling.lastChild;
+        if (previous == null) {
           return
         }else {
-          Table.focus(previous)
+          previous.click();
         }
       }
     }else{
-      Table.focus(cell.previousSibling.previousSibling)
+      cell.previousSibling.click();
     }
-  },
+  }
+
   //导出表格数据
-  export : function(table,props) {
+  function exportData(el,props) {
     var prop = props;
-    prop.row = [];
-    //导出指定行的数据
-    GetRowData = function(a){
-      var rowData = table.getElementsByTagName('tr')[a];//获取当前行
-      var cells= rowData.getElementsByTagName('td');//获取当前行的单元格
-      props.row[a]=[];
-      for (var j = 0; j < props.title.length; j++){
-        props.row[a][j] = cells[j].innerHTML;
+    var table = el.getElementsByTagName('table');
+    for(var i = 0; i < prop.length; i++) {
+      prop[i].row = [];
+      var rowLength = table[i].getElementsByTagName('tr').length-2;
+      for (var q = 0; q < rowLength; q++) {
+        //导出指定行的数据
+        GetRowData = function(a) {
+          var rowData = table[i].getElementsByTagName('tr')[a];//获取当前行
+          var cells= rowData.getElementsByTagName('td');//获取当前行的单元格
+          prop[i].row[a]=[];
+          for (var j = 0; j < props[i].title.length-1; j++) {
+            prop[i].row[a][j] = (cells[j+1].innerHTML);
+          }
+        }
+        prop[i].row.push(GetRowData(q+2));
       }
     }
-    for (var i = 0; i < props.title.length; i++) {
-      prop.row.push(GetRowData(i+1));
-    }
+    console.log(prop);
     return prop;
   }
-};
+})(window)
+
+//用法
+var box = document.getElementById('box');
+props=[
+  {
+    head:'本周工作任务及完成',
+    title:['序号','工作任务描述','起止日期','完成率％','备注'],
+    row:[
+      [1,2,3,4],
+      [3,2,4,1]
+    ],
+  },
+  {
+    head:'本周工作任务及完成情况',
+    title:['序号','工作任务描述','起止日期','备注'],
+    row:[
+    ],
+  }
+]
+var Table = init(box,props);
