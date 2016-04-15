916a5366cdd21a735a4d0263f49618271cbcd824
Peach <scdzwyxst@gmail.com>
2016-04-15 09:14:05 +0800

    Delete useless file

--- utils.js
+++ utils.js
@@ -1,69 +1,126 @@
-;(function (global) {
-  var doc = null;
-  function isHaveShift(key) {
-    for(var i in key) {
-      if (key[i] === 16) {
-        key.splice(key[i], 1);
-        return true;
-      }
+((global) => {
+  const utils = {};
+
+  /**
+   * 快捷键绑定
+   * @param  {DOM}      element  绑定元素
+   * @param  {String}   key      绑定按键，形如 'shift+tab'
+   * @param  {Function} callback 回调函数
+   * @return {Object}            链式操作
+   */
+  utils.bind = (element, key, callback) => {
+    const strategies = {
+      shift: 'shiftKey',
+      tab: 9,
+      enter: 13,
+      backspace: 8,
+    };
+
+    if (!element.bindKeys) {
+      element.bindKeys = {};
     }
-    return false;
-  }
-  global.utils = {
-    bind: function(element, key, callback) {
-      if (!element || !key) {
-        throw new Error('参数不能为空');
-      }
 
-      doc = element.addEventListener('keydown', function(event) {
+    element.bindKeys[key] = (e) => {
+      const keys = key.split('+');
+      const ev = e || window.event;
+      let temp = true;
+      let i;
+      let strategy;
 
-        if (key.length === 1) {
-          if (event.shiftKey === false && event.keyCode === key[0]) {
-            callback.call(null, this);
-          }
-        } else if (key.length > 1) {
-          if (isHaveShift(key)) {
-            if (event.shiftKey === true && event.keyCode === key[0]) {
-              callback.call(null, this);
+      for (i in strategies) {
+        if (strategies.hasOwnProperty(i)) {
+          strategy = strategies[i];
+          if (keys.indexOf(i) > -1) {
+            if (typeof(strategy) === 'number' && strategy !== ev.keyCode) {
+              temp = false;
+            } else if (typeof(strategy) === 'string' && !ev[strategy]) {
+              temp = false;
             }
+          } else if (typeof(strategy) === 'string' && ev[strategy]) {
+            temp = false;
           }
         }
-        // end if
-      })
-
-      return callback;
-    },
-
-    unbind: function (element, key, callback) {
-      element.removeEventListener('keydown', doc);
-
-      return callback;
-    },
-
-    copyElement: function (element) {
-      var range = document.createRange();
-      range.selectNodeContents(element);
-      window.getSelection().addRange(range);
-
-      try {
-        var style = document.execCommand('copy');
-        var msg = style ? '成功' : '失败';
-        alert('拷贝'+msg);
-      } catch(err) {
-        alert('不能拷贝');
       }
-      window.getSelection().removeAllRanges();
-    },
 
-    enableAutoSave: function (gap) {
-      setInterval(function() {
-        localStorage.setItem('saves', table.export());
-      }, gap * 1000);
-    },
+      if (temp && callback) {
+        callback.call(this, ev);
+      }
+    };
 
-    enableLoadData: function () {
-      return localStorage.getItem('saves');
+    element.addEventListener('keydown', element.bindKeys[key]);
+
+    return this;
+  };
+
+  /**
+   * 快捷键解绑
+   * @param  {DOM}      element  绑定元素
+   * @param  {String}   key      绑定按键，形如 'shift+tab'
+   * @param  {Function} callback 回调函数
+   * @return {Object}            链式操作
+   */
+  utils.unbind = (element, key, callback) => {
+    if (element.bindKeys[key]) {
+      element.removeEventListener('keydown', element.bindKeys[key]);
+      if (callback) {
+        callback.call(this);
+      }
     }
+
+    return this;
+  };
+
+  /**
+   * 复制元素
+   * @param  {DOM}      element  绑定元素
+   */
+  utils.copyElement = (element) => {
+    const customAlert = alert;
+    let range = null;
+
+    window.getSelection().removeAllRanges();
+    range = document.createRange();
+    range.selectNodeContents(element);
+    window.getSelection().addRange(range);
+    try {
+      const style = document.execCommand('copy');
+      customAlert(`拷贝${style === true ? '成功' : '失败'}`);
+    } catch (err) {
+      customAlert('不能拷贝');
+    }
+    window.getSelection().removeAllRanges();
+  };
+
+  /**
+   * 保存到本地数据库
+   * @param  {Integer}  gap   秒数
+   * @param  {Function} data  回调函数
+   */
+  utils.save = (gap, data) => {
+    setInterval(() => {
+      localStorage.setItem('saves', data());
+    }, gap * 1000);
+  };
+
+  /**
+   * 从本地数据库读取
+   *
+   */
+  utils.load = () => localStorage.getItem('save');
+
+  /**
+   * 封装confirm方法
+   *
+   */
+  utils.confirm = (data) => {
+    const customConfirm = confirm;
+    return customConfirm(data);
+  };
+
+
+  if (global.utils) {
+    throw new Error('There has utils already, cannot export utils!');
+  } else {
+    global.utils = utils;
   }
 })(window);
-
