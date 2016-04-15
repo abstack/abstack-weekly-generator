bb5e7b552713b573dc6b03f77b3e9baeb04a4290
Hellocyy <Hellocyy94824@163.com>
2016-04-15 11:05:04 +0800

    Modify export for table.js

--- table.js
+++ table.js
@@ -4,13 +4,20 @@
   }
   const weeklyTable = {};
 
-  // 获取聚焦单元格
+  /**
+   * 获取聚焦单元格
+   * @return {Dom} 当前聚焦单元格
+   */
   function getFocusCell() {
     const input = document.getElementsByTagName('input')[0];
     return input ? input.parentNode : input;
   }
 
-  // 查找父级表格
+  /**
+   * 查找父级表格
+   * @param  {DOM} elm  当前元素
+   * @return {Dom} elm  父级表格
+   */
   function findTableParent(cell) {
     if (cell) {
       const parent = cell.parentNode;
@@ -22,7 +29,10 @@
     return undefined;
   }
 
-  // 创建新的一行
+  /**
+   * 创建新行
+   * @param  {DOM} elm  当前元素
+   */
   function createNewLine(cell) {
     const tr = document.createElement('tr');
     const table = findTableParent(cell);
@@ -37,7 +47,10 @@
     tr.getElementsByTagName('td')[1].click();
   }
 
-  // 删除当前行
+  /**
+   * 删除当前行
+   * @param  {DOM} elm  当前元素
+   */
   function deleteLine(cell) {
     const tr = cell.parentNode;
     const table = tr.parentNode;
@@ -54,7 +67,10 @@
     });
   }
 
-  // 编辑数据
+  /**
+   * 编辑数据
+   * @param  {DOM} elm  当前元素
+   */
   function write(cell) {
     cell.addEventListener('click', (e) => {
       const input = document.createElement('input');
@@ -89,7 +105,9 @@
     });
   }
 
-  // 将输入框移到下一个单元格
+  /**
+   * 将输入框移到下一个单元格
+   */
   function focusNextCell() {
     const cell = getFocusCell();
     if (cell) {
@@ -106,7 +124,9 @@
     }
   }
 
-  // 将输入框移到上一个单元格
+  /**
+   * 将输入框移到上一个单元格
+   */
   function focusPrevCell() {
     const cell = getFocusCell();
     // 如果是每行第一个
@@ -126,16 +146,32 @@
       cell.previousSibling.click();
     }
   }
-  // 获取行数据
+
+  /**
+   * 获取行数据
+   * @param  {DOM} elm    当前行
+   * @return {Array} roaData  行数据
+   */
   function getRowData(row) {
     const rowData = [];
     const cells = row.getElementsByTagName('td');
-    Array.prototype.forEach.call(cells, (cell) => {
-      rowData.push(cell.innerHTML);
+    Array.prototype.forEach.call(cells, (cell, index) => {
+      if (index > 0) {
+        if (cell.getElementsByTagName('input').length !== 0) {
+          rowData.push(cell.getElementsByTagName('input')[0].value);
+        } else {
+          rowData.push(cell.innerHTML);
+        }
+      }
     });
     return rowData;
   }
-  // 获取表数据
+
+  /**
+   * 获取表数据
+   * @param  {DOM} elm    当前表
+   * @return {Array} TableData  表数据
+   */
   function getTableData(table) {
     const TableData = [];
     const rows = table.getElementsByTagName('tr');
@@ -144,7 +180,12 @@
     });
     return TableData.slice(2);
   }
-  // 获取页面所有表数据
+
+  /**
+   * 获取所有表数据
+   * @param  {DOM} elm    当前所有表
+   * @return {Array} prop  所有表数据
+   */
   function exportData(el, props) {
     const prop = props;
     const tableData = [];
@@ -155,8 +196,15 @@
     prop.forEach((item, index) => {
       item.row = tableData[index];
     });
-    return JSON.stringify(prop);
+    return prop;
   }
+
+  /**
+   * 初始化函数
+   * @param  {DOM} elm    容器元素
+   * @param  {Array} props 表格配置
+   * @return {weeklyTable}
+   */
   weeklyTable.init = (box, props) => {
     props.forEach((item) => {
       const table = document.createElement('table');
@@ -164,6 +212,8 @@
       const header = document.createElement('tr');
       const headerTxt = document.createElement('td');
       const title = document.createElement('tr');
+      const number = document.createElement('td');
+      const numberTxt = document.createElement('td');
       // 表格
       box.appendChild(table);
 
@@ -176,6 +226,9 @@
 
       // 表头
       table.appendChild(title);
+      number.innerHTML = '序号';
+      number.className = 'table-title';
+      title.appendChild(number);
       item.title.forEach((titlename) => {
         const titleTxt = document.createElement('td');
         titleTxt.className = 'table-title';
@@ -186,6 +239,7 @@
       if (item.row.length === 0) {
         const cells = document.createElement('tr');
         table.appendChild(cells);
+        cells.appendChild(numberTxt);
         item.title.forEach(() => {
           const cellTxt = document.createElement('td');
           cells.appendChild(cellTxt);
@@ -195,10 +249,9 @@
       } else {
         item.row.forEach((row, index) => {
           const cells = document.createElement('tr');
-          const number = document.createElement('td');
-          number.innerHTML = index + 1;
+          numberTxt.innerHTML = index + 1;
           table.appendChild(cells);
-          cells.appendChild(number);
+          cells.appendChild(numberTxt);
           row.forEach((cell) => {
             const cellTxt = document.createElement('td');
             cellTxt.innerHTML = cell;
@@ -208,7 +261,9 @@
         });
       }
     });
-    utils.save(3, () => exportData(box, props));
+    return {
+      exportData: () => exportData(box, props),
+    };
   };
 
   if (global.weeklyTable) {
