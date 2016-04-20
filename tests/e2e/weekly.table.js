4fbfc090ee93a9212f08cc8eca472537b5638ebe
Hellocyy <Hellocyy94824@163.com>
2016-04-20 14:34:13 +0800

    Add test for addLine and deleteLine

--- tests/e2e/weekly.table.js
+++ tests/e2e/weekly.table.js
@@ -1,6 +1,5 @@
-casper.test.begin('weekly table', 4, function (test) {
+casper.test.begin('weekly table', 6, function (test) {
   var startSelector = '#table-container table:nth-child(1) tr:nth-child(3) td:nth-child(2)';
-
   // 打开网页
   casper.start('http://localhost:8080/', function () {
     // 检查 HTTP 状态
@@ -10,6 +9,10 @@ casper.test.begin('weekly table', 4, function (test) {
   casper.waitForSelector(startSelector, function () {
     var testText = 'This is a test text!';
     var nextSelector = '#table-container table:nth-child(1) tr:nth-child(3) td:nth-child(3)';
+    var Firtstcell = '#table-container table:nth-child(1) tr:last-child td:first-child';
+    var first;
+    var before;
+    var after;
     casper.click(startSelector);
 
     // 输入测试文字
@@ -30,8 +33,19 @@ casper.test.begin('weekly table', 4, function (test) {
 
       // 检查焦点是否回到第一个单元格
       test.assertExists(startSelector + ' input', 'Cell has been toggled back successfully');
+      first = this.getHTML(Firtstcell);
+      //按下enter键
+      casper.sendKeys(startSelector, casper.page.event.key.Enter);
+      before = this.getHTML(Firtstcell) - 1 +'';
+      //检查新增行是否起效
+      test.assertEquals(before, first, 'Line has been added successfully');
+
+      // 按下 Shift+backspace 键
+      casper.sendKeys(startSelector, casper.page.event.key.Backspace, {modifiers: 'shift'});
+      after = this.getHTML(Firtstcell);
+      //检查删除行是否起效
+      test.assertEquals(before, after, 'Line has been deleted successfully');
     });
   });
-
   casper.run();
 });
